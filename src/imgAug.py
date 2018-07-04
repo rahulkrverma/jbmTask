963bf7e34067d71a8538f2af92e795c681474a94
Rahul Verma <rahulkrverma@hotmail.com>
2018-07-04 16:02:29 +0530

    image preprocessing opencv

--- src/imgAug.py
+++ src/imgAug.py
@@ -36,7 +36,7 @@ def imgDilation(imgArr):
 if __name__ == '__main__':
 	
 	dataPath = '../All_61326/train_61326/'
-	augPath = '/home/user/Desktop/jbmTask/imgAug'
+	augPath = '/home/user/Desktop/jbmTask/imgAug_edge'
 
 	rt_mat = [5, -1, 10, -10]
 
@@ -55,25 +55,70 @@ if __name__ == '__main__':
 					imgPath = os.path.join(classPath, img)
 
 					imgArr = cv2.imread(imgPath, 0)
-					imgArr = cv2.resize(imgArr, (224, 224), interpolation=cv2.INTER_CUBIC)
+					# imgArr = cv2.resize(imgArr, (720, 480), interpolation=cv2.INTER_CUBIC)
 
-					imgPadd = padding(imgArr)
-					for i in range(4):
-						imgRt = rotateImg(imgPadd, rt_mat[i])
-						imgEros = imgErrode(imgRt)
-						imgDial = imgDilation(imgRt)
+					# blur = cv2.medianBlur(imgArr, 5)
+					# blur = cv2.GaussianBlur(imgArr,(5,5),0)
+
+					# th3 = cv2.adaptiveThreshold(imgArr_1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 2)
+					# edges = cv2.Canny(imgArr, 50, 50)
+					
+					# kernel = np.ones((5,5),np.float32)/17
+					# dst = cv2.filter2D(imgArr,-1,kernel)
+
+					# blur = cv2.blur(imgArr,(5,5))
+					# edges = cv2.Canny(blur, 50, 70)
+
+					img_gray = cv2.bilateralFilter(imgArr, 15, 19, 19)
+					# img_gray = cv2.GaussianBlur(imgArr, (3, 3), 0)
+					# img_edge = cv2.Canny(img_gray, 10, 50)
+
+					# img_edge = cv2.Laplacian(imgArr ,cv2.CV_64F)
+
+					img_edge = cv2.Sobel(img_gray,cv2.CV_64F, 0, 1, ksize=5)
+					
+					# ret, th1 = cv2.threshold(img_edge, 100, 255,cv2.THRESH_BINARY)
+
+					th3 = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 25, 3)
+
+					# _, cnts, _ = cv2.findContours(img_edge.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+					# cnts = sorted(cnts, key = cv2.contourArea, reverse = True)
+					# outline = np.zeros(imgArr.shape, dtype = "uint8")
+					# cv2.drawContours(outline, cnts, -1, (255, 255, 255), -1)
+
+					tempPath = os.path.join(classAug, img)
+					cv2.imwrite(tempPath, th3)
+
+
+					# cv2.imshow('fdvsfdvfd', sobelx)
+					# cv2.waitKey()
+				
+
+					# cv2.imwrite('lap.jpg', dst)
+					# cv2.imwrite('x.jpg', sobelx)
+					# cv2.imwrite('y.jpg', sobely)
+
+
+
+
+
+			# 		imgPadd = padding(imgArr)
+			# 		for i in range(4):
+			# 			imgRt = rotateImg(imgPadd, rt_mat[i])
+			# 			imgEros = imgErrode(imgRt)
+			# 			imgDial = imgDilation(imgRt)
 						
-						imgName = img.split('.')[0]
-						rtName = imgName + '_' + str(rt_mat[i]) + '.jpg'
-						errodeName = imgName + '_' + str(rt_mat[i]) + '_errode.jpg'
-						dialName = imgName + '_' + str(rt_mat[i]) + '_dilation.jpg'
+			# 			imgName = img.split('.')[0]
+			# 			rtName = imgName + '_' + str(rt_mat[i]) + '.jpg'
+			# 			errodeName = imgName + '_' + str(rt_mat[i]) + '_errode.jpg'
+			# 			dialName = imgName + '_' + str(rt_mat[i]) + '_dilation.jpg'
 
-						rtPath = os.path.join(classAug, rtName)
-						errodePath = os.path.join(classAug, errodeName)
-						dialPath = os.path.join(classAug, dialName)
+			# 			rtPath = os.path.join(classAug, rtName)
+			# 			errodePath = os.path.join(classAug, errodeName)
+			# 			dialPath = os.path.join(classAug, dialName)
 
-						cv2.imwrite(rtPath, imgRt)
-						cv2.imwrite(errodePath, imgEros)
-						cv2.imwrite(dialPath, imgDial)
+			# 			cv2.imwrite(rtPath, imgRt)
+			# 			cv2.imwrite(errodePath, imgEros)
+			# 			cv2.imwrite(dialPath, imgDial)
 
-			print('Images written for class,\t', className)
+			# print('Images written for class,\t', className)
